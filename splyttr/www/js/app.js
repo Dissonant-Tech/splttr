8d6402648c069c9a85778402fe4892e8e68327a8
martindidiego <didiegomartin@gmail.com>
2016-08-19 19:25:38 -0400

    Reconfigured API endpoints and resposne status checks

--- splyttr/www/js/app.js
+++ splyttr/www/js/app.js
@@ -21,7 +21,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       StatusBar.style(1);
 
     }
-    $rootScope.token = "Token 9944b09199c62bcf9418ad846dd0e4bbdfc6ee4b"
   });
 })
 
