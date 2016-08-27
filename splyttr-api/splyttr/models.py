37bdd159dc811ef6203091fa00b49697276b47e5
Alan Kopetman <akope001@fiu.edu>
2016-08-26 22:59:14 -0400

    added user/id/activity: returns all of a users bills and event names

--- splyttr-api/splyttr/models.py
+++ splyttr-api/splyttr/models.py
@@ -16,8 +16,9 @@ class Tab(models.Model):
     description = models.CharField(
         max_length=240
     )
-    created = models.DateField(
-        auto_now_add=True
+    created_at = models.DateTimeField(
+            '%m/%d/%Y %H:%M',
+            auto_now_add=True,
     )
     members = models.ManyToManyField(User)
 
@@ -29,14 +30,14 @@ class Event(models.Model):
     description = models.CharField(
         max_length=240
     )
-    created = models.DateField(
-        auto_now_add=True
+    created_at = models.DateTimeField(
+            '%m/%d/%Y %H:%M',
+            auto_now_add=True,
     )
     tab = models.ForeignKey(
         Tab,
         on_delete=models.CASCADE
     )
-
 
 class Bill(models.Model):
     creditor = models.ForeignKey(
@@ -58,4 +59,8 @@ class Bill(models.Model):
         on_delete=models.CASCADE
     )
     amount = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateTimeField(
+            '%m/%d/%Y %H:%M',
+            auto_now_add=True,
+    )
     is_paid = models.BooleanField(default=False)
