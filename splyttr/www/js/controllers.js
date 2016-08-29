0218c5441584ae91f41da7a62c59e4f341723760
martindidiego <didiegomartin@gmail.com>
2016-08-28 20:55:16 -0400

    Added angular charts module

--- splyttr/www/js/controllers.js
+++ splyttr/www/js/controllers.js
@@ -422,6 +422,14 @@ angular.module('starter.controllers', [])
   // load user data before entering home state
   $scope.$on("$ionicView.beforeEnter", function(event, data){
 
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+      $scope.series = ['Series A', 'Series B'];
+
+      $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+      ];
+
     // Get user data
     if($stateParams.userId === 'self'){
       User.get().then(function(user){
